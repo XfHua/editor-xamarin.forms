4c608c6c52c0c0f9051975d8290d6c6d677da817
XfHua <jasonHuaxf@163.com>
2019-06-13 11:16:04 +0800

    123

---
+++ App405/App405/App405.iOS/WrapperStackLayoutRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using App405;
+using App405.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly: ExportRenderer(typeof(WrapperStackLayout), typeof(WrapperStackLayoutRenderer))]
+namespace App405.iOS
+{
+    public class WrapperStackLayoutRenderer : VisualElementRenderer<StackLayout>
+    {
+        public WrapperStackLayoutRenderer()
+        {
+            UIKeyboard.Notifications.ObserveWillShow((sender, args) =>
+            {
+                if (Element != null)
+                {
+                    Element.Margin = new Thickness(0, 0, 0, (args.FrameEnd.Height));
+                }
+            });
+
+            UIKeyboard.Notifications.ObserveWillHide((sender, args) =>
+            {
+                if (Element != null)
+                {
+                    Element.Margin = new Thickness(0); //set the margins to zero when keyboard is dismissed
+                }
+            });
+        }
+    }
+}
---
+++ App405/App405/App405/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App405
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class EditorWithAutoSize : Editor
+    {
+        public EditorWithAutoSize()
+        {
+            this.TextChanged += (sender, e) =>
+            {
+                this.InvalidateMeasure();
+            };
+        }
+    }
+
+    public class WrapperStackLayout : StackLayout
+    {
+    }
+}
